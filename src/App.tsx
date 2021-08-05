f5c8af3bded4f3446e3d86526d1cec71ca4bff85
Rosiee7 <88431787+Rosiee7@users.noreply.github.com>
2021-08-05 20:29:44 +0300

    Add antd design and movies nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mockData from './mock';
+import MoviesNav from './MoviesNav';
 
 function App() {
+  console.log(mockData.results);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MoviesNav movies={mockData.results} />
     </div>
   );
 }
